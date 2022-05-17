9f857d3ed870478f25f735bc53318a687959da3d
souhir812 <souhir.benboubakar@gmail.com>
2022-05-17 21:03:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/Profile';
+import ProfilePhoto from './res/ProfilePhoto.jpg';
+
 
 function App() {
+  let fullName= "souhir ben boubaker ";
+  let bio=" i am a biomedical ingeneering student specilized in embeddded electronic system.I am very intersted in this domain so to improve my skills i shoose to learn web developpment with GOMYCODE.I am so happy with this teamwork specillay my moniteur and my freinds."
+  let profession=" web developer";
+  const handleName=()=>alert(` welcome to know more details about ${fullName}`)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile fullName={fullName} bio={bio} profession={profession} handleName={handleName}>
+        <img style={{marginTop:"6%", width:"25%", borderRadius:"50%"}} src={ProfilePhoto} alt='My pic Profile'/>
+      </Profile>
     </div>
   );
 }
---
+++ src/profile/Profile.js
@@ -0,0 +1,50 @@
+import propTypes from 'prop-types';
+
+const position = {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap"
+}
+const customStyle = {
+    fontFamily: "Sacramento",
+    fontSize: "5rem",
+    color: "pink",
+    textAlign: "center",
+    margin: "1%"
+}
+const bioAndprofession = {
+    color: "black",
+    lineHeight: "1.7",
+    fontSize: "1.3rem",
+    fontWeight: "normal",
+    textAlign: "center"
+}
+
+const Profile = ({fullName, bio, profession,handleName,children}) => {
+  return (
+    <div style={position}>
+      {handleName(fullName)}
+      {children}     
+      <div>
+        <h1 style={customStyle}>{fullName}</h1>
+        <h2 style={bioAndprofession}>{bio}</h2>
+        <p style={bioAndprofession}>{profession}</p>
+      </div>
+    </div>
+  )
+}
+
+Profile.defaultProps={
+    fullName: "Tell us your Name",
+    bio: "Tell us your Bio",
+    profession:"Tell us your Profession",
+    handleName:"undefind alert"
+}
+Profile.propTypes={
+    fullName: propTypes.string,
+    bio: propTypes.string,
+    profession: propTypes.string,
+    handleName:propTypes.func
+}
+
+export default Profile;
